0d20992cc989d00fd94131f2ba972478776396e0
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-04 00:34:03 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      if self.name== meal.customer.name
+        #binding.pry
+        meal
+      end
+    end
+  end
+
+  def waiters
+    Waiter.all.select do |waiter|
+      #binding.pry
+      if customer.waiter== self
+        #binding.pry
+        waiter
+      end
+    end
+  end
+
+
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,29 @@
+require "pry"
+
 class Waiter
+
+  attr_accessor :name, :years, :meals
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+
+  def best_tipper
+
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def meals
+    Meal.all
+  end
 
 end
